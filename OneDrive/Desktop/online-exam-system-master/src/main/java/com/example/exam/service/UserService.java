e09b9128b38a94ffb7618d8ebe6edbcaea05954d
pulkitkandpal5215 <pulkitkandpal5215@gmail.com>
2026-03-24 10:44:44 +0530

    Delete OneDrive/Desktop/online-exam-system-master directory

--- OneDrive/Desktop/online-exam-system-master/src/main/java/com/example/exam/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.exam.service;
-
-import com.example.exam.model.User;
-
-public interface UserService {
-    void saveStudent(User user);
-    User findByUsername(String username);
-}
